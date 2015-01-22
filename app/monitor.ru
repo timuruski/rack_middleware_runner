38f0a7fd4123ab5bea298f9532b1e46eb2b88a3e
Tim Uruski <github@tmu.pw>
2015-01-21 22:36:30 -0700

    Sketch out rough runner app

--- app/monitor.ru
+++ app/monitor.ru
@@ -1,8 +1,22 @@
-app = Proc.new do |env|
-  headers = {'Content-type' => 'application/json'}
-  body = ['{"name": "Monitor"}']
+require_relative '../environment'
+require 'sinatra'
+require 'json'
 
-  [200, headers, body]
+class MonitorApp < Sinatra::Base
+  get '/users' do
+    JSON.dump({users: Users.all})
+  end
+
+  get '/reports/:id' do
+    report_id = params.fetch('id')
+    # current_user = request.env.fetch('rack.current_user')
+    # if report = Reports.find_for_user(current_user, report_id)
+    if report = Reports.find(report_id)
+      JSON.dump({report: report})
+    else
+      not_found
+    end
+  end
 end
 
-run app
+run MonitorApp.new
--- environment.rb
+++ environment.rb
@@ -1,41 +1,65 @@
 require 'bundler/setup'
+require 'pathname'
+require 'yaml'
 
 $LOAD_PATH.unshift File.expand_path '../lib', __FILE__
 
-class User; end
-class UserRepo
-  def initialize(users = {})
-    @users = users
+class Model
+  def self.new(*attrs)
+    Struct.new(*attrs) do
+      def self.load(attrs)
+        attrs.each_with_object(new) { |(key, value), obj|
+          obj.send("#{key}=", value) }
+      end
+
+      yield if block_given?
+    end
+  end
+end
+
+class Repository
+  def self.load(model, path)
+    yaml = File.read File.expand_path(path)
+    objects = YAML.load(yaml).map { |attrs| model.load(attrs) }
+
+    repo = new(objects)
+    class << repo
+      yield if block_given?
+    end
+
+    repo
   end
 
+  def initialize(objects)
+    @objects = objects
+  end
+
+  def find(id)
+    id = id.to_i
+    @objects.find { |obj| obj.id == id }
+  end
+
+  def all
+    @objects.to_a
+  end
+end
+
+class User < Model.new(:id, :name, :token); end
+class Report < Model.new(:id, :user_id, :title); end
+
+Users = Repository.load(User, 'data/users.yml') do
   def find_by_token(token)
-    @users[token]
+    token = token.to_s
+    @objects.find { |user| user.token == token }
   end
 end
 
-__END__
-
-require 'sequel'
-
-class User < Struct.new(:name, :email, :token)
-  alias_method :to_json, :to_h
-  class << self
-    REPO = [
-      new('alice', 'alice@example.org', 'abc123'),
-      new('bob', 'bob@example.org', 'def234'),
-      new('charlie', 'charlie@example.org', 'ghi345')
-    ]
-
-    def find(id)
-      REPO[id - 1]
-    end
-
-    def find_by_token(token)
-      REPO.find { |user| user.token == token }
-    end
+Reports = Repository.load(Report, 'data/reports.yml') do
+  def find_for_user(user, report_id)
+    user_id = user.id
+    @objects.find { |report|
+      report.user_id == user_id &&
+        report.id == report_id
+    }
   end
-end
-
-class Post < Struct.new(:title, :body)
-  alias_method :to_json, :to_h
 end
